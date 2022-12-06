a3c4985779a4224031fc32c59139fca1960cc4ce
eyoul <49184150+eyoul@users.noreply.github.com>
2022-12-07 02:59:28 +0300

    pyfix

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """Defines the HBNB console."""
 import cmd
 from shlex import split
-from models import Filestorage
+from models import storage
 from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
