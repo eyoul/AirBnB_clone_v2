088248cf7c718e3bad5c5b5f953644fe905e50c0
eyoul <eyoulshimeles2@gmail.com>
2022-12-07 17:50:25 +0000

    Repo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,12 +5,11 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["35.237.92.54", "3.238.90.226"]
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
-
     Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
